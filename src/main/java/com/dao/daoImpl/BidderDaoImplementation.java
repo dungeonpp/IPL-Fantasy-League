9f3f56d93332d6885182fdcd2c86234a7ce742d9
ronmatt11 <ronmatt11@gmail.com>
2017-09-25 19:25:35 +0530

    added showtodaysMatch() in existing code

--- src/main/java/com/dao/daoImpl/BidderDaoImplementation.java
+++ src/main/java/com/dao/daoImpl/BidderDaoImplementation.java
@@ -73,13 +73,44 @@ public class BidderDaoImplementation implements BidderDao
 		factory.close();
 	}
 	
+@Override
+	public void showTodaysMatch(Date d)
+	{
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();		
+		Transaction tx = session.beginTransaction();
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		String todaysDate = sdf.format(d);
+		
+		
+		String SQL_QUERY = "From Match match";
+		Query query = session.createQuery(SQL_QUERY);
+		for(Iterator it = query.iterate();it.hasNext();)
+		{
+			Match match = (Match)it.next();
+			Date matchDate1=match.getMatchDateTime();
+			String matchDate = sdf.format(matchDate1);
+			if(todaysDate.equalsIgnoreCase(matchDate))
+			{
+				System.out.println(match);
+			}
+		}
+	
+		tx.commit();
+		session.close();
+		
+	}
 	
 	
 	
 	public static void main(String[] args) 
 	{
 		BidderDaoImplementation bdi = new BidderDaoImplementation();
-		
+		Date d = new Date();
+		bdi.showTodaysMatch(d);
 		bdi.updateProfile(1);  // bidderId
 		bdi.updateBid(1, 1, 2);  // bidId, matchId, teamId 
 	}
