355618b78b9186a93bd31787eea247c9f4cbd93a
Smujahid <luckymujahid.09@gmail.com>
2017-09-26 17:55:27 +0530

    Added updateMatch() Method and Solve  addmatch() method problem

--- src/main/java/com/dao/daoimpl/AdminDaoImplementation.java
+++ src/main/java/com/dao/daoimpl/AdminDaoImplementation.java
@@ -27,9 +27,70 @@ import com.model.Team;
 public class AdminDaoImplementation implements AdminDao {
 
 	static List<Bidder> bidder=new ArrayList<>();
-	
+	@Override
+	public void updateMatches(int matchId,String dateTime) {
+		Session session = Connection.getSession();
+        Transaction t = session.beginTransaction();
+		String query="update Match set match_date_time=\'"+dateTime+"\' where match_id="+matchId;
+		Query q=session.createQuery(query);
+		q.executeUpdate();
+		t.commit();
+		System.out.println(session.get(Match.class,1));
+		session.close();
+		Connection.shutdown();
+	}
 
-	
+	@Override
+	public void addMatches() {
+		try{
+			
+			FileInputStream input = new FileInputStream("C:\\Users\\Administrator\\Desktop\\ipl_match.xls");
+			POIFSFileSystem fs = new POIFSFileSystem( input );
+			HSSFWorkbook wb = new HSSFWorkbook(fs);
+			HSSFSheet sheet = wb.getSheetAt(0);
+			Row row;
+			for(int i=1; i<=sheet.getLastRowNum(); i++){
+				Session session = Connection.getSession();
+		        Transaction t = session.beginTransaction();
+				row = sheet.getRow(i);
+				Date date = row.getCell(0).getDateCellValue();         
+				SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				String op4=sdf3.format(date);
+				String teamA = row.getCell(1).getStringCellValue();
+				String teamB = row.getCell(2).getStringCellValue();
+				String matchwinner = row.getCell(3).getStringCellValue();
+		       
+				List<Team> teamOne=new ArrayList<Team>();
+				List<Team> teamTwo=new ArrayList<Team>();
+				teamOne=session.createQuery("from Team where teamName=\'"+teamA+"\'").list();
+				teamTwo=session.createQuery("from Team where teamName=\'"+teamB+"\'").list();
+				Match match=new Match();
+				
+				int teamAId=teamOne.get(0).getTeamId();
+				int teamBId=teamTwo.get(0).getTeamId();
+				Team team1= new Team();
+				Team team2=new Team();
+				team1=(Team) session.get(Team.class,teamAId);
+				team2=(Team) session.get(Team.class,teamBId);
+				match.setMatchDateTime(date);
+				match.setTeam1(team1);
+				match.setTeam2(team2);
+				match.setMatchWinner("Draw");
+				session.save(match);
+				t.commit();
+				session.close();
+			
+			}
+			
+			
+			Connection.shutdown();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+
+
+	}
+
 	
 	
 
